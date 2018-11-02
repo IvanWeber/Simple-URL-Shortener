92889ec8169a683c96c86edec92524b7c2742d7d
Ivan <ivanbdn@yandex.ru>
2018-11-02 04:48:27 +0300

    Созданы html и css файлы проекта

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Сокращатель ссылок</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+    <h1 class="main-header">Сократить ссылку</h1>
+    <form action="shorten.php" method="post">
+        <input type="url" name=url" placeholder="Введите вашу ссылку" autocomplete="off">
+        <input type="submit" value="Сократить">
+    </form>
+</div>
+</body>
+</html>
